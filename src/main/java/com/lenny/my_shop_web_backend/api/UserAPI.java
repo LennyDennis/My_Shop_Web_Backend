dcc0ee236333adec0f93161d294120dbc584cc03
LennyDennis <lennydennis@gmail.com>
2021-05-11 14:07:09 +0300

    Create user registration endpoint

--- src/main/java/com/lenny/my_shop_web_backend/api/UserAPI.java
+++ src/main/java/com/lenny/my_shop_web_backend/api/UserAPI.java
@@ -5,10 +5,36 @@
  */
 package com.lenny.my_shop_web_backend.api;
 
+import com.lenny.my_shop_web_backend.ejb.UserBean;
+import com.lenny.my_shop_web_backend.entities.User;
+import com.lenny.my_shop_web_backend.utilities.JsonResponse;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author lenny
  */
+@Stateless
+@Path("/user")
+@Produces({MediaType.APPLICATION_JSON})
 public class UserAPI {
     
+    @EJB
+    UserBean userBean;
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response registerUser(User user) {
+        JsonResponse response = userBean.registerUser(user);
+        return Response.ok(response.getResponseCode()).entity(response).build();
+
+    }
+
 }
