cc7e6c4feb529cefe505caa8d5116cfc6d52136f
LennyDennis <lennydennis@gmail.com>
2021-05-11 09:00:45 +0300

    Add JsonResponse utility class

---
+++ src/main/java/com/lenny/my_shop_web_backend/api/UserAPI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lenny.my_shop_web_backend.api;
+
+/**
+ *
+ * @author lenny
+ */
+public class UserAPI {
+    
+}
---
+++ src/main/java/com/lenny/my_shop_web_backend/utilities/JsonResponse.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lenny.my_shop_web_backend.utilities;
+
+/**
+ *
+ * @author lenny
+ */
+public class JsonResponse {
+    
+    private Integer code;
+    private String message;
+
+    public JsonResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
