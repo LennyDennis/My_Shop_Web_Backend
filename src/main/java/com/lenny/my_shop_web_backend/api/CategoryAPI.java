982d2ba7ec9bfc5413fa028794ed08b2dea7dd03
LennyDennis <lennydennis@gmail.com>
2021-05-19 21:24:08 +0300

    Add endpoints to edit and delete category

--- src/main/java/com/lenny/my_shop_web_backend/api/CategoryAPI.java
+++ src/main/java/com/lenny/my_shop_web_backend/api/CategoryAPI.java
@@ -9,12 +9,15 @@ import com.lenny.my_shop_web_backend.ejb.CategoryBean;
 import com.lenny.my_shop_web_backend.entities.Category;
 import com.lenny.my_shop_web_backend.entities.User;
 import com.lenny.my_shop_web_backend.utilities.JsonResponse;
+import java.util.HashMap;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -26,13 +29,27 @@ import javax.ws.rs.core.Response;
 @Path("/category")
 @Produces({MediaType.APPLICATION_JSON})
 public class CategoryAPI {
-    
+
     @EJB
     CategoryBean categoryBean;
 
     @POST
     public Response addCategory(Category category) {
         JsonResponse response = categoryBean.addCategory(category);
+        return Response.ok(response.getResponseCode()).entity(response).build();
+    }
+
+    @PUT
+    @Path("edit")
+    public Response editCategory(@QueryParam("categoryId") Integer categoryId, HashMap hashMap) {
+        JsonResponse response = categoryBean.editCategory(categoryId, hashMap);
+        return Response.ok(response.getResponseCode()).entity(response).build();
+    }
+
+    @PUT
+    @Path("delete")
+    public Response deleteCategory(@QueryParam("categoryId") Integer categoryId) {
+        JsonResponse response = categoryBean.deleteCategory(categoryId);
         return Response.ok(response.getResponseCode()).entity(response).build();
     }
 
