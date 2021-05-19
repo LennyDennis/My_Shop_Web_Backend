978b7ba95a131b3460348111cbea125549a7d3dd
LennyDennis <lennydennis@gmail.com>
2021-05-19 21:24:37 +0300

    Add logic to edit and delete a category

--- src/main/java/com/lenny/my_shop_web_backend/ejb/CategoryBean.java
+++ src/main/java/com/lenny/my_shop_web_backend/ejb/CategoryBean.java
@@ -10,7 +10,9 @@ import com.lenny.my_shop_web_backend.entities.Category;
 import com.lenny.my_shop_web_backend.jpa.TransactionProvider;
 import static com.lenny.my_shop_web_backend.utilities.ConstantVariables.*;
 import com.lenny.my_shop_web_backend.utilities.JsonResponse;
+import com.lenny.my_shop_web_backend.utilities.ValuesFromHashMap;
 import java.util.Date;
+import java.util.HashMap;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -33,15 +35,78 @@ public class CategoryBean {
         try {
             if (category != null) {
                 Category existingCategory = categoryDatabaseBean.getCategory_ByName(category.getName());
-                if (existingCategory == null) {
-                    category.setAddedDate(currentDate);
-                    category.setDeletionStatus(NOT_DELETED);
-                    if (provider.createEntity(category)) {
-                        response.setResponseCode(SUCCESS_CODE);
-                        response.setMessage("Category " + category.getName() + " has been created");
+                if (existingCategory == null || existingCategory.getDeletionStatus() == DELETED) {
+                    if (existingCategory.getDeletionStatus() == DELETED) {
+                        existingCategory.setAddedDate(currentDate);
+                        existingCategory.setDeletionStatus(NOT_DELETED);
+                        if (provider.createEntity(existingCategory)) {
+                            response.setResponseCode(SUCCESS_CODE);
+                            response.setMessage("Category " + existingCategory.getName() + " has been created");
+                        }
+                    } else {
+                        category.setAddedDate(currentDate);
+                        category.setDeletionStatus(NOT_DELETED);
+                        if (provider.createEntity(category)) {
+                            response.setResponseCode(SUCCESS_CODE);
+                            response.setMessage("Category " + category.getName() + " has been created");
+                        }
                     }
+
                 } else {
                     response.setMessage("Category " + category.getName() + " already exists");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return response;
+        }
+    }
+
+    public JsonResponse editCategory(Integer categoryId, HashMap hashMap) {
+        JsonResponse response = new JsonResponse(ERROR_CODE, ERROR_MESSAGE);
+        try {
+            if (hashMap != null) {
+                String categoryName = ValuesFromHashMap.getStringValue_FromHashMap(hashMap, "name");
+                Category existingCategory = categoryDatabaseBean.getCategory_ByName(categoryName);
+                if (existingCategory == null) {
+                    Category categoryToEdit = categoryDatabaseBean.getCategory_ById(categoryId);
+                    if (categoryToEdit != null) {
+                        String originalName = categoryToEdit.getName();
+                        categoryToEdit.setName(categoryName);
+                        if (provider.updateEntity(categoryToEdit)) {
+                            response.setResponseCode(SUCCESS_CODE);
+                            response.setMessage("Category name has been updated to " + categoryToEdit.getName());
+                        }
+                    }
+                } else {
+                    response.setMessage("Category with name " + categoryName + " already exists");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return response;
+        }
+    }
+
+    public JsonResponse deleteCategory(Integer categoryId) {
+        JsonResponse response = new JsonResponse(ERROR_CODE, ERROR_MESSAGE);
+        try {
+            if (categoryId != null) {
+                Category categoryToDelete = categoryDatabaseBean.getCategory_ById(categoryId);
+                if (categoryToDelete != null) {
+                    if (categoryToDelete.getDeletionStatus() != DELETED) {
+                        categoryToDelete.setDeletionStatus(DELETED);
+                        if (provider.updateEntity(categoryToDelete)) {
+                            response.setResponseCode(SUCCESS_CODE);
+                            response.setMessage("Category " + categoryToDelete.getName() + " has been deleted");
+                        }
+                    } else {
+                        response.setMessage("Category has already been deleted");
+                    }
+                } else {
+                    response.setMessage("Category does not exist");
                 }
             }
         } catch (Exception e) {
